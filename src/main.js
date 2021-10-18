91b8b0c0f2d27c8037b1be6c019dda82aba0a9b0
GuillaumeBergeronGeoffroy <geoffroy_5@hotmail.com>
2021-10-18 17:50:12 -0400

    moving

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import GlobalMixin from './mixins/Global.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
+  mixins: [GlobalMixin],
   router,
   render: h => h(App)
 }).$mount('#app')
---
+++ src/mixins/Global.vue
@@ -0,0 +1,17 @@
+<script>
+    export default {
+        name: 'GlobalMixin',
+
+        data: function() {
+            return {
+                allCookies: document.cookie
+            }
+        },
+        methods: {
+
+        },
+        mounted() {
+
+        },
+    }
+</script>
